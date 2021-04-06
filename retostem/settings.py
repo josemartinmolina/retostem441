e2450ed7decfeb687ab06206eee6a008bfc3d3f6
José Martín Molina <75451788+josemartinmolina@users.noreply.github.com>
2021-04-06 15:55:14 -0500

    Update settings.py

--- retostem/settings.py
+++ retostem/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wx_0d@=^@nz^^tnmk3%5&zesc%in(5u+lt!!v$!%brzqbs-j&&'
+with open(BASE_DIR/'SECRET_KEY') as f:
+SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

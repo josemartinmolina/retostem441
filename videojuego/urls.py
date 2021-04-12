f8922a5521145c1dfa022b29b9c5069af13545c5
josemartinmolina <jose.molina@tec.mx>
2021-04-12 16:41:18 -0500

    Creacion servicios

--- videojuego/urls.py
+++ videojuego/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('',views.index, name = 'index'),
     path('proceso',views.proceso, name = 'proceso'),
     path('datos',views.datos, name = 'datos'),
+    path('unity',views.unity, name = 'unity'),
+    path('buscaJugadorBody',views.buscaJugadorBody, name = 'buscaJugadorBody'),
 ]
--- videojuego/views.py
+++ videojuego/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from json import loads
 from . models import Reto
 
 # Create your views here.
@@ -16,3 +18,24 @@ def proceso(request):
 def datos(request):
     jugadores = Reto.objects.all() #select * from Reto;
     return render(request, 'datos.html', {'lista_jugadores':jugadores})
+
+@csrf_exempt
+def unity(request):
+    nombre = "Martin"
+    score = "1234"
+    retorno = {"nombreUsuario":nombre,
+        "score":score}
+    return JsonResponse(retorno)
+
+@csrf_exempt
+def buscaJugadorBody(request):
+    body_unicode = request.body.decode('utf-8')
+    body_json = loads(body_unicode) #convertir de string a JSON
+    jugador_nombre = body_json['usuario']
+    resultados = Reto.objects.filter(nombre=jugador_nombre)  #select * from Reto where nombre = jugador_nombre
+    nombre = resultados[0].nombre
+    score = resultados[0].minutos_jugados
+    retorno = {"nombreUsuario":nombre,
+        "score":score}
+    return JsonResponse(retorno)
+
